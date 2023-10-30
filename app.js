fc6a1ea1fc7ee6c543e5fb9182fc420f15d6b5fb
sophia <sophiakim.dev@gmail.com>
2023-10-29 22:42:18 -0400

    nav and iframe added

---
+++ app.js
@@ -0,0 +1,6 @@
+function tracking() {
+    var iframe = document.getElementById('display');
+    if (iframe.style.display === "none" || iframe.style.display === "") {
+        iframe.style.display = "block";
+    }
+}
